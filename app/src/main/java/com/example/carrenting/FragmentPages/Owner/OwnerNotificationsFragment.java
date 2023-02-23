8faae00e5c8ac5a8c49a06018d30bd3f56d37a44
AnhTrung732 <93305749+AnhTrung732@users.noreply.github.com>
2023-02-24 00:25:53 +0700

    Add customer and owner activities

--- app/src/main/java/com/example/carrenting/FragmentPages/Owner/OwnerNotificationsFragment.java
+++ app/src/main/java/com/example/carrenting/FragmentPages/Owner/OwnerNotificationsFragment.java
@@ -1,21 +1,20 @@
 package com.example.carrenting.FragmentPages.Owner;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.example.carrenting.R;
 
-public class OwnerBankingFragment extends Fragment {
+public class OwnerNotificationsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.owner_fragment_banking, container, false);
+        return inflater.inflate(R.layout.owner_fragment_notification, container, false);
     }
 }
